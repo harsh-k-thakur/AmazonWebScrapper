8b3eee519b904b36012bb2a142cc93f986f4e50f
harry-kt <HThakur@ucsd.edu>
2022-03-30 10:03:45 -0700

    Basic Scrapping

---
+++ code/password.py
@@ -0,0 +1,17 @@
+USERNAME = "Harsh.Kishorsingh.Thakur@gmail.com"
+PASSWORD = "################"
+
+# Usually if you have two factor on for your email or gmail account
+# Create an app password, else it raise an error
+# Python won't be able to access the gmail.
+
+# In order to create an app password.
+    # 1. Go to Google Account
+    # 2. Security
+    # 3. Signing in with Google
+    # 4. Create App Password.
+    
+# From here things should be intiutive
+# There will be 16 digit password for specific app will be generated
+# The email id will be same as your, don't get confused with the email id shown there on the app password page
+# And the password with 16 character long, you have to use it without spaces in python.
